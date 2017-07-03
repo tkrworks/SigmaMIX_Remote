b73062894fecce8dbd7d03acf226defe85207343
tkrworks <info@tkrworks.net>
2017-07-04 07:13:43 +0900

    Add the gpl license comments.

--- app/src/main/java/net/tkrworks/sigmamixremote/MyTextViewControl.java
+++ app/src/main/java/net/tkrworks/sigmamixremote/MyTextViewControl.java
@@ -1,11 +1,28 @@
+/*
+ * Copylight (C) 2017, Shunichi Yamamoto, tkrworks.net
+ *
+ * This file is part of SigmaMIX Remote.
+ *
+ * SigmaMIX Remote is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option ) any later version.
+ *
+ * SigmaMIX Remote is distributed in the hope that it will be useful,
+ * but WITHIOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SigmaMIX Remote. if not, see <http:/www.gnu.org/licenses/>.
+ *
+ * MyTextViewControl.java
+ */
+
 package net.tkrworks.sigmamixremote;
 
 import android.widget.TextView;
 import java.util.Locale;
-
-/**
- * Created by shun on 2017/06/28.
- */
 
 public class MyTextViewControl {
   public static void setDecibel(TextView textview, int val, int dBMin, int dBMax) {
